016587f8d45d2c82e8a03ae24061f0918fbdd92a
ddanyliuk18 <ddanyliuk@kse.org.ua>
2025-06-15 22:41:10 +0300

    Users implemented

---
+++ DB-Assignment-4/Scripts/users.sql
@@ -0,0 +1,14 @@
+USE assignment4;
+
+        CREATE USER 'news_agent'@'assignment_4' IDENTIFIED BY 'superqwerty123';
+        GRANT SELECT ON assignment4.results TO 'news_agent'@'assignment_4';
+
+        
+        CREATE USER 'admin'@'assignment_4' IDENTIFIED BY 'analysisentry';
+        GRANT SELECT ON assignment4.* TO 'admin'@'assignment_4';
+
+        
+        CREATE USER 'lawyer'@'assignment_4' IDENTIFIED BY 'superqwerty123';
+        GRANT SELECT ON assignment4.athlete_passports TO 'lawyer'@'assignment_4';
+        
+FLUSH PRIVILEGES;
